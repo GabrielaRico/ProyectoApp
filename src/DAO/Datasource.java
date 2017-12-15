d462087ff2fb9d5aca91b08b6a33da20624bce40
gabyrico <gabyrico@10.102.0.57>
2017-12-15 16:26:21 -0600

    fix sql

--- src/DAO/Datasource.java
+++ src/DAO/Datasource.java
@@ -11,20 +11,20 @@ public class Datasource {
 	    private String bd;
 	       
 	public Datasource(String hostname, String puerto, String usuario, String password, String bd){
-	    this.hostname=hostname;
-	    this.puerto=puerto;
-	    this.usuario=usuario;
-	    this.password=password;
-	    this.bd=bd;
-	    
+	    this.hostname="clariantbd.database.windows.net";
+	    this.usuario="gaby";
+	    this.password="Alki1380";
+	    this.bd="clariantbd";
 	    
 	}
 	public Connection getConexion(){
 	    
 	    Connection c=null;
+        String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostname, bd, usuario, password);
+
 	    try{
 	        Class.forName("com.sqlserver.jdbc.Driver");
-	        c=DriverManager.getConnection("jdbc:sqlserver://"+hostname+":"+puerto+"/"+bd,usuario,password);
+	        c=DriverManager.getConnection(url);
 	        }
 	    catch(Exception e){
 	        e.printStackTrace();
